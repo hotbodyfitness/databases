119d2f76504500c909f6494f1afc1d5ef3d4a047
Shawn Joyce <shawn.joyce@gmail.com>
2019-06-27 12:44:27 -0500

    Make commit

--- server/schema.sql
+++ server/schema.sql
@@ -4,7 +4,27 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  text varchar(200) NOT NULL,
+  roomid int NOT NULL, /* FIX */
+  PRIMARY KEY (id),
+  FOREIGN KEY (userid) REFERENCES users(id),
+  FOREIGN KEY (roomid) REFERENCES rooms(id)
 );
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(15),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT,
+  roomname varchar(15),
+  PRIMARY KEY (id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
