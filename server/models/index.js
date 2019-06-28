6c7ad3ba20d43b6c996fe7fefc5e26039e9b9196
Shawn Joyce <shawn.joyce@gmail.com>
2019-06-27 22:55:54 -0500

    Make Thursday commit

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,22 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (request, response) {  // a function which produces all the messages
+      models.messages.get((err, data) => {
+        console.log('data: ', data);
+        console.log('response: ', response);
+        if (err) {
+          response.status(500);
+          response.send('We got an error 500!');
+        } else {
+          response.status(200);
+          response.type('json');
+          response.send(JSON.stringify({data}));
+        }
+      });
+    },
+
+    post: function (request, response) {} // a function which can be used to insert a message into the database
   },
 
   users: {
