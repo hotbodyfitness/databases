708fffc0fcde5a75c4ac0a2e868d072517ad19dc
Zack Miller <hotbodyfitness@gmail.com>
2019-07-04 09:06:42 -0700

    Complete refactoring with Sequelize

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,62 +1,102 @@
-var models = require("../models");
+// var models = require("../models");
+var db = require("../db");
+
+// SEQUELIZE refactor Completely removes the need for Server —> Models
 
 module.exports = {
   messages: {
-    get: function(req, res) { // a function which handles a get request for all messages
-      models.messages.get((err, data) => {
-        console.log("data: ", data);
-        if (err) {
-          console.log("Error from controllers.get :", err);
-          // res.status(500);
-          // res.send('We got an error 500!');
-        } else {
-          // res.status(200);
-          // res.type("json");
-          res.send(JSON.stringify({ data }));
-        }
-      });
+    get: function (req, res) { // a function which handles a get request for all messages
+
+      // SEQUELIZE refactor
+      db.Messages.findAll() // .findAll({include: [User]}) would Left Outer Join the Users table {join:}(would be inner join)
+        .complete((err, data) => {
+          res.json(data); // same as = res.send(JSON.stringify({ data }))
+        });
+
+      // models.messages.get((err, data) => { // THIS BLOCK was before refactor
+      //   console.log("data: ", data);
+      //   if (err) {
+      //     console.log("Error from controllers.get :", err);
+      //     // res.status(500);
+      //     // res.send('We got an error 500!');
+      //   } else {
+      //     // res.status(200);
+      //     // res.type("json");
+      //     res.send(JSON.stringify({ data }));
+      //   }
+      // });
     },
-    post: function(req, res) { // a function which handles posting a message to the database
-      var obj = {
-        username: req.body.username,
-        message: req.body.message,
-        roomname: req.body.roomname
-      };
-      console.log("POST Request.body :", req.body);
-      models.messages.post(obj, (err, data) => {
-        if (err) {
-          console.log("Error from controllers.post :", err);
-        } else {
-          console.log("DATA :", data.message);
-          res.sendStatus(201);
-        }
-      });
+    post: function (req, res) { // a function which handles posting a message to the database
+      // SEQUELIZE refactor
+      // .findOrCreate - will find the room if exists, or create it if it doesn't
+      db.Users.findOrCreate({username: req.body.username}) // access to id from USERS table
+        .complete((err, user) => {
+          db.Rooms.findOrCreate({roomname: req.body.roomname}) // access to id from ROOMS table
+            .complete((err, room) => {
+              var obj = {
+                userid: user.id,
+                message: req.body.message,
+                roomid: room.id
+              };
+
+              db.Messages.create(obj)
+                .complete((err, data) => {
+                  res.sendStatus(201);
+                });
+            });
+        });
+
+      // var obj = {                   // THIS BLOCK was before refactor
+      //   username: req.body.username,
+      //   message: req.body.message,
+      //   roomname: req.body.roomname
+      // };
+      // models.messages.post(obj, (err, data) => {
+      //   if (err) {
+      //     console.log("Error from controllers.post :", err);
+      //   } else {
+      //     console.log("DATA :", data.message);
+      //     res.sendStatus(201);
+      //   }
+      // });
     }
   },
 
   users: {
-    get: function(req, res) {
-      models.users.get((err, data) => {
-        // console.log("data: ", data);
-        if (err) {
-          console.log("Error from controllers.get :", err);
-        } else {
-          res.send(JSON.stringify({ data }));
-        }
-      });
+    get: function (req, res) {
+      // SEQUELIZE refactor
+      db.Users.findAll()
+        .complete((err, data) => {
+          res.json(data); // same as = res.send(JSON.stringify({ data }))
+        });
+
+      // models.users.get((err, data) => {
+      //   // console.log("data: ", data);
+      //   if (err) {
+      //     console.log("Error from controllers.get :", err);
+      //   } else {
+      //     res.send(JSON.stringify({ data }));
+      //   }
+      // });
     },
-    post: function(req, res) {
-      var obj = {
-        username: req.body.username
-      };
-      console.log("POST Request.body :", req.body);
-      models.users.post(obj, (err, data) => {
-        if (err) {
-          console.log("Error from controllers.post :", err);
-        } else {
+    post: function (req, res) {
+      // SEQUELIZE refactor
+      db.Users.create({username: req.body.username})
+        .complete((err, data) => {
           res.sendStatus(201);
-        }
-      });
+        });
+
+      // var obj = {
+      //   username: req.body.username
+      // };
+      // console.log("POST Request.body :", req.body);
+      // models.users.post(obj, (err, data) => {
+      //   if (err) {
+      //     console.log("Error from controllers.post :", err);
+      //   } else {
+      //     res.sendStatus(201);
+      //   }
+      // });
     }
   }
 };
--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,63 @@
-var mysql = require('mysql');
-// SEQUELIZE refactor
-var Sequelize = require('sequelize');
-var db = new Sequelize('chat', 'root', '');
-
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-// for var mysql - no longer used
-var connection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: 'chat'
-});
-// connection.connect();
+// SEQUELIZE refactor
+var Sequelize = require('sequelize');
+var db = new Sequelize('chat', 'root', '');
 
+var Users = db.define('Users', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: Sequelize.STRING
+});
+var Messages = db.define('Messages', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  message: Sequelize.STRING,
+  roomid: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: 'rooms',
+      key: 'id'
+    }
+  },
+  userid: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: 'users',
+      key: 'id'
+    }
+  }
+});
+var Rooms = db.define('Rooms', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  roomname: Sequelize.STRING
+});
+
+// this is how FOREIGN KEYS are defined
+Users.hasMany(Messages);
+Rooms.hasMany(Messages);
+Messages.belongsTo(Users, { foreignKey: 'userid' });
+Messages.belongsTo(Rooms, { foreignKey: 'roomid' });
+
+// Syncronize these Sequelize var's
+Users.sync();
+Messages.sync();
+Rooms.sync();
 
 // the following 6 lines are just there to verify that the connection works
 db.authenticate()
@@ -25,8 +68,17 @@ db.authenticate()
     console.error('SEQUELIZE Unable to connect to the database:', err);
   });
 
-module.exports = db;
+module.exports.Users = Users;
+module.exports.Messages = Messages;
+module.exports.Rooms = Rooms;
 
-// for var mysql - no longer used
+// FOLLOWING lines were for mysql without Sequelize - no longer needed
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   user: 'root',
+//   password: '',
+//   database: 'chat'
+// });
+// connection.connect();
+
 // module.exports = connection;
-
